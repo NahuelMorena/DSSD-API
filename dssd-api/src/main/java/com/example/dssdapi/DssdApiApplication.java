9e5135ffff71c48935f78353232f7dc25b4140a0
Hernan <hernanleiva1998@gmail.com>
2023-10-14 14:41:11 -0300

    Spring App

---
+++ dssd-api/src/main/java/com/example/dssdapi/DssdApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dssdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssdApiApplication.class, args);
+	}
+
+}
