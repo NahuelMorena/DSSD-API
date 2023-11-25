b0301b3cf6b46b6a25798c9c4e7e7bf25f356650
matiasboni <matiasboni01@gmail.com>
2023-11-25 17:08:16 -0300

    Correction in query

--- src/main/java/com/example/dssdapi/repositories/ProviderOffersMaterialRepository.java
+++ src/main/java/com/example/dssdapi/repositories/ProviderOffersMaterialRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.dssdapi.model.ProviderOffersMaterial;
 
 public interface ProviderOffersMaterialRepository extends CrudRepository<ProviderOffersMaterial,Long> {
-	 @Query("SELECT p FROM ProviderOffersMaterial p WHERE LOWER(p.material.name) = LOWER(:materialName) and p.delivery_date_available <= :dateStartManufacture and p.quantity_available != 0")
+	 @Query("SELECT p FROM ProviderOffersMaterial p WHERE LOWER(p.material.name) = LOWER(:materialName) and p.delivery_date_available <= :dateStartManufacture and p.quantity_available != 0"
+	 		+ " and p.delivery_date_available > CURRENT_DATE")
 	    List<ProviderOffersMaterial> findOffersByMaterialName(String materialName,LocalDate dateStartManufacture);
 }
