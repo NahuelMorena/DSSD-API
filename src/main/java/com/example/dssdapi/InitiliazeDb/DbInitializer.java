e10f8ccba5c83f0ac72b7eb57ab69762a6da39b1
matiasboni <matiasboni01@gmail.com>
2023-11-25 18:04:34 -0300

    Correction

--- src/main/java/com/example/dssdapi/InitiliazeDb/DbInitializer.java
+++ src/main/java/com/example/dssdapi/InitiliazeDb/DbInitializer.java
@@ -69,13 +69,13 @@ public class DbInitializer implements ApplicationRunner {
 			this.providerRepository.save(provider1);
 			ProviderOffersMaterial po1=new ProviderOffersMaterial(provider1, material1, 10, LocalDate.of(2023, 11, 5),Float.valueOf(150));
 			this.providerOffersMatRepository.save(po1);
-			Material material2 = new Material("Plástico", new HashSet<>(), new HashSet<>());
+			Material material2 = new Material("Plastico", new HashSet<>(), new HashSet<>());
 	        Material material3 = new Material("Vidrio", new HashSet<>(), new HashSet<>());
 	        Material material4 = new Material("Papel", new HashSet<>(), new HashSet<>());
 	        this.materialRepository.saveAll(List.of(material2, material3, material4));
 	        Provider provider2 = new Provider(RoleType.RECICLADOR, new HashSet<>(), new HashSet<>(), "Proveedor2", "222-2222222", "Proveedor2@gmail.com");
 	        this.providerRepository.save(provider2);
-	        ProviderOffersMaterial po2 = new ProviderOffersMaterial(provider1, material2, 20, LocalDate.of(2023, 11, 10), Float.valueOf(100));
+	        ProviderOffersMaterial po2 = new ProviderOffersMaterial(provider1, material2, 20, LocalDate.of(2023, 11, 11), Float.valueOf(100));
 	        ProviderOffersMaterial po3 = new ProviderOffersMaterial(provider1, material3, 15, LocalDate.of(2023, 11, 7), Float.valueOf(90));
 	        ProviderOffersMaterial po4 = new ProviderOffersMaterial(provider2, material4, 30, LocalDate.of(2023, 11, 15), Float.valueOf(80));
 	        this.providerOffersMatRepository.saveAll(List.of(po2, po3, po4));
